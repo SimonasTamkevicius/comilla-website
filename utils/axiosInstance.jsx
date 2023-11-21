adffe56dafb5d646c63dcaf3c0fd9ac17c4717f8
Simonas Tamkevicius <simonas.ta@icloud.com>
2023-11-21 17:31:39 -0500

    fix for single event spacing issue

--- utils/axiosInstance.jsx
+++ utils/axiosInstance.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const backendBaseURL = "https://unusual-jade-jeans.cyclic.app"
+const backendBaseURL = "https://unusual-jade-jeans.cyclic.app"
 // const backendBaseURL = "https://comilla-website-backend.onrender.com";
-const backendBaseURL = "http://localhost:9000";
+// const backendBaseURL = "http://localhost:9000";
 
 const axiosInstance = axios.create({
     baseURL: backendBaseURL,
