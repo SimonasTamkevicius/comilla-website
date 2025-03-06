179bca90184699fe9748baf5992c2fca7a2bedba
Simonas Tamkevicius <simonas.ta@icloud.com>
2025-03-05 22:08:44 -0500

    fixed backend url

--- utils/axiosInstance.jsx
+++ utils/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const backendBaseURL = "https://unusual-jade-jeans.cyclic.app"
-const backendBaseURL = "https://comilla-website-backend.onrender.com";
+const backendBaseURL = "https://nebulous-pine-look.glitch.me/";
 // const backendBaseURL = "http://localhost:9000";
 
 const axiosInstance = axios.create({
